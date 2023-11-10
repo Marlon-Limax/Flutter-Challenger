e7f4ee1271b12d00c5de6fc0254cbcb2db56d66e
Marlon-Limax <marlonlimax2012@gmail.com>
2023-11-10 18:11:47 -0300

    Creating Screen | custom_text_field.dart

---
+++ lib/src/common_widgets/custom_text_field.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class _CustomTextFieldState extends StatelessWidget {
+  const _CustomTextFieldState({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
