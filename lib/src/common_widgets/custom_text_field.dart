b51f76540943cf6b9f0f0a5dd6bf5dfbf1a7476a
Marlon-Limax <marlonlimax2012@gmail.com>
2023-11-10 19:07:42 -0300

    creating login screen design, and customizing the textform through custom_text_field.dart

--- lib/src/common_widgets/custom_text_field.dart
+++ lib/src/common_widgets/custom_text_field.dart
@@ -1,10 +1,74 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-class _CustomTextFieldState extends StatelessWidget {
-  const _CustomTextFieldState({super.key});
+class CustomTextField extends StatefulWidget {
+  final IconData icon;
+  final bool isSecret;
+  final List<TextInputFormatter>? inputFormatters;
+  final String? initialValue;
+  final bool readOnly;
+  final Color backgroundColor; // Adicione esta propriedade
+
+  const CustomTextField({
+    Key? key,
+    required this.icon,
+    this.isSecret = false,
+    this.inputFormatters,
+    this.initialValue,
+    this.readOnly = false,
+    this.backgroundColor = Colors.white, // Cor padrão para branco
+  }) : super(key: key);
+
+  @override
+  State<CustomTextField> createState() => _CustomTextFieldState();
+}
+
+class _CustomTextFieldState extends State<CustomTextField> {
+  bool isObscure = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    isObscure = widget.isSecret;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 15),
+      child: Container(
+        decoration: BoxDecoration(
+          color: widget.backgroundColor, // Defina a cor de fundo aqui
+          borderRadius: BorderRadius.circular(5),
+        ),
+        child: TextFormField(
+          readOnly: widget.readOnly,
+          initialValue: widget.initialValue,
+          inputFormatters: widget.inputFormatters,
+          obscureText: isObscure,
+          decoration: InputDecoration(
+            prefixIcon: Icon(widget.icon, color: Colors.black), // Ícone preto
+            suffixIcon: widget.isSecret
+                ? IconButton(
+                    onPressed: () {
+                      setState(() {
+                        isObscure = !isObscure;
+                      });
+                    },
+                    icon: Icon(isObscure ? Icons.visibility : Icons.visibility_off),
+                    color: Colors.black, // Ícone preto
+                  )
+                : null,
+            isDense: true,
+            contentPadding: EdgeInsets.zero, // Remova o preenchimento interno
+            border: OutlineInputBorder(
+              borderSide: BorderSide.none, // Remova a borda
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/src/pages/auth/sign_in_screen.dart
+++ lib/src/pages/auth/sign_in_screen.dart
@@ -1,10 +1,139 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_challenger/src/common_widgets/custom_text_field.dart';
+import 'package:flutter_challenger/src/pages/auth/home/home_tab.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class SignInScreen extends StatelessWidget {
-  const SignInScreen({super.key});
+  const SignInScreen({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 50.0),
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                Color(0xFF1A4752),
+                Color.fromRGBO(41, 152, 142, 1.0),
+              ],
+            ),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center, // Ajustado para começar do centro
+            children: [
+              // Envolver os elementos superiores em Expanded
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start, // Ajustado para começar da esquerda
+                  children: [
+                    // Label Usuário
+                    const Padding(
+                      padding: EdgeInsets.only(bottom: 5, left: 5), // Adicionado espaçamento e ajuste para a direita
+                      child: Text(
+                        'Usuário',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                    // email
+                    const CustomTextField(
+                      icon: Icons.person,
+                    ),
+
+                    // Label Senha
+                    const Padding(
+                      padding: EdgeInsets.only(
+                          top: 10, bottom: 5, left: 5), // Adicionado espaçamento e ajuste para a direita
+                      child: Text(
+                        'Senha',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                    // senha
+                    const CustomTextField(
+                      icon: Icons.lock,
+                      isSecret: true,
+                    ),
+
+                    // Espaçamento entre TextFields e Botão
+                    const SizedBox(height: 20),
+
+                    // Botão de entrar centralizado
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        SizedBox(
+                          height: 50,
+                          width: 150,
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.green,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(24),
+                              ),
+                            ),
+                            onPressed: () {
+                              Navigator.of(context).pushReplacement(
+                                MaterialPageRoute(
+                                  builder: (c) {
+                                    return const HomeTab();
+                                  },
+                                ),
+                              );
+                            },
+                            child: const Text(
+                              'Entrar',
+                              style: TextStyle(fontSize: 18),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+
+              // Padding apenas para a Label "Política de Privacidade"
+              Padding(
+                padding: const EdgeInsets.only(
+                    bottom: 20), // Adicionado espaçamento abaixo da Label "Política de Privacidade"
+                child: TextButton(
+                  onPressed: () => openUrl(),
+                  child: const Text(
+                    "Política de Privacidade",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void openUrl() async {
+    const url = 'https://google.com.br/';
+    if (await canLaunchUrl(Uri.parse(url))) {
+      await launchUrl(Uri.parse(url));
+    } else {
+      throw 'Could not launch $url';
+    }
   }
 }
